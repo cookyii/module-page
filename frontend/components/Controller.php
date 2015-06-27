112225d17f583aafdb079631afca32303f747cd4
Revin Roman <xgismox@gmail.com>
2015-06-27 14:33:03 +0300

    Update

---
+++ frontend/components/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Controller.php
+ * @author Alexander Volkov
+ */
+
+namespace cookyii\modules\Page\frontend\components;
+
+/**
+ * Class Controller
+ * @package cookyii\modules\Page\frontend\components
+ */
+abstract class Controller extends \frontend\components\Controller
+{
+
+}
